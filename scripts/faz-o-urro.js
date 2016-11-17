b8bc39461ca2e22f5520b8fcb4da1c827dfcf98d
Victor Perin <victor.perin@princiweb.com.br>
2016-11-17 14:10:20 -0200

    fix(urro): funcionamento no telegram

--- scripts/faz-o-urro.js
+++ scripts/faz-o-urro.js
@@ -18,7 +18,7 @@
 'use strict';
 
 module.exports = function (robot){
-  robot.hear(/^(faz o (\w*)ro)( \d+)?( \d+)?$/i, function (res){
+  robot.hear(/(faz o (\w*)ro)( \d+)?( \d+)?$/i, function (res){
     const tipoDeUrro = res.match[2];
     const numeroDeUrros = res.match[3] || 10;
     const delayEntreUrros = res.match[4] || 3000;
